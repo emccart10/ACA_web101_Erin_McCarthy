dc5fda4e85734ed027d4b958c1bcef8c34973fec
Emccart10 <esmccarthy@gmail.com>
2019-03-02 15:02:29 -0600

    updated to-dolist

--- tic-tac-toe/ticTac.js
+++ tic-tac-toe/ticTac.js
@@ -1,12 +1,14 @@
 function func1() {
-    alert("I made a pop-up");
-  }
-  window.onload=func1;
-  
-
-  newimg = sprite_add("/mario.png",1,false,false,0,0);
-
-  with (instance_create(x, y, obj_blank)) {
-    index = 1;
-    instance_change(obj_player, true);
+  alert("I made a pop-up");
 }
+window.onload=func1;
+
+
+// newimg = sprite_add("/mario.png",1,false,false,0,0);
+
+$(function() {
+$('.toad').click(function(){
+  $("#bg").attr('src',"/mario.png");
+  return false;
+});
+});
